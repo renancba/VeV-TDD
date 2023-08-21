f229ee9e45893c394a39728ebd969dda2ea4d48e
arthuralv <arthur.alves@ccc.ufcg.edu.br>
2023-08-21 14:39:38 -0300

    Adicionando JUnit e criando o primeiro teste

---
+++ processador_de_boletos/test/TestProcessadorBoletos.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertTrue;
+
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class TestProcessadorBoletos {
+    
+    @Test
+    public void testFaturaPagaComBoletos() {
+        Fatura fatura = new Fatura("Cliente", 1500.0, "10-04-2017");
+        List <Boleto> boletos = Arrays.asList(
+            new Boleto(1, "10-04-2017", 249.5),
+            new Boleto(2, "10-04-2017", 400.0),
+            new Boleto(3, "10-04-2017", 849.5),
+            new Boleto(4, "10-04-2017", 1.0)
+        );
+
+        ProcessadorBoletos processador = new ProcessadorBoletos();
+        processador.processarBoletos(fatura, boletos);
+
+        assertTrue(fatura.isPaga());
+    }
+}
