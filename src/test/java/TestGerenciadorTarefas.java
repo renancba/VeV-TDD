60054da90a1f4d554ca78e5a7002796e8b93a178
Renan Carneiro <55746096+renancba@users.noreply.github.com>
2023-08-21 21:55:53 -0300

    criando test inicial do gerenciador

--- src/test/java/TestGerenciadorTarefas.java
+++ src/test/java/TestGerenciadorTarefas.java
@@ -1,12 +1,21 @@
+import org.example.controller.GerenciadorTarefas;
 import org.example.model.Tarefa;
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.time.LocalDate;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestGerenciadorTarefas {
+
+    private GerenciadorTarefas gerenciador;
+
+    @BeforeEach
+    public void init() {
+        this.gerenciador = new GerenciadorTarefas();
+    }
 
     @Test
     public void testCriarTarefa() {
@@ -15,6 +24,13 @@ public class TestGerenciadorTarefas {
         assertEquals("fazer testagem usando tdd", tarefaTest.getDescricao());
         assertEquals( LocalDate.of(2018, 9, 28), tarefaTest.getDataVencimento());
         assertEquals("alta", tarefaTest.getPrioridade());
+    }
+
+    @Test
+    public void testGerenciadorTarefa(){
+        Tarefa tarefaTest = new Tarefa("fazer testes", "fazer testagem usando tdd", LocalDate.of(2018, 9, 28), "alta");
+        gerenciador.adicionar(tarefaTest);
+        assertTrue(1, gerenciador.listarTarefas().size());
     }
 
 }
