96ff5ce318fd7ac2e874f5ebd1cb9890479cf50c
Renan Carneiro <55746096+renancba@users.noreply.github.com>
2023-08-21 21:49:53 -0300

    criando enum de tarefa e o database

---
+++ src/main/java/org/example/repository/GerenciadorData.java
@@ -0,0 +1,26 @@
+package org.example.repository;
+
+import org.example.model.Tarefa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerenciadorData {
+    private final List<Tarefa> tarefas;
+
+    public GerenciadorData() {
+        this.tarefas = new ArrayList<>();
+    }
+
+    public void addTarefa(Tarefa tarefa){
+        tarefas.add(tarefa);
+    }
+
+    public List<Tarefa> getTarefas() {
+        return tarefas;
+    }
+
+    public void removerTarefa(Tarefa tarefa){
+        tarefas.remove(tarefa);
+    }
+}
