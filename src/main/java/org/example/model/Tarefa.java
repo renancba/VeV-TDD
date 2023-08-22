356491e764b18d063e7a2c4b3a9b7433f8dd9766
Renan Carneiro <55746096+renancba@users.noreply.github.com>
2023-08-21 21:35:09 -0300

    criando tarefa

---
+++ src/main/java/org/example/model/Tarefa.java
@@ -0,0 +1,63 @@
+package org.example.model;
+
+import java.time.LocalDate;
+
+public class Tarefa {
+
+    private String titulo;
+
+    private String descricao;
+
+    private LocalDate dataVencimento;
+
+    private String prioridade;
+
+    public Tarefa(String titulo, String descricao, LocalDate dataVencimento, String prioridade) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataVencimento = dataVencimento;
+        this.prioridade = prioridade;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public String getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", dataVencimento=" + dataVencimento +
+                ", prioridade='" + prioridade + '\'' +
+                '}';
+    }
+}
--- src/test/java/TestGerenciadorTarefas.java
+++ src/test/java/TestGerenciadorTarefas.java
@@ -1,10 +1,20 @@
+import org.example.model.Tarefa;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestGerenciadorTarefas {
 
     @Test
     public void testCriarTarefa() {
-        Tarefa tarefaTest = new Tarefa("fazer testes", "fazer testagem usando tdd", "29-03-2019", "alta");
+        Tarefa tarefaTest = new Tarefa("fazer testes", "fazer testagem usando tdd", LocalDate.of(2018, 9, 28), "alta");
+        assertEquals("fazer testes", tarefaTest.getTitulo());
+        assertEquals("fazer testagem usando tdd", tarefaTest.getDescricao());
+        assertEquals( LocalDate.of(2018, 9, 28), tarefaTest.getDataVencimento());
+        assertEquals("alta", tarefaTest.getPrioridade());
     }
 
 }
