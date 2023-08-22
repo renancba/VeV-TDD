a07a9b228ceb9768462f811d26ed990c62a3fa4e
Renan Carneiro <55746096+renancba@users.noreply.github.com>
2023-08-21 21:12:35 -0300

    first commit and initial tests

---
+++ src/test/java/TestGerenciadorTarefas.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class TestGerenciadorTarefas {
+
+    @Test
+    public void testCriarTarefa() {
+        Tarefa tarefaTest = new Tarefa("fazer testes", "fazer testagem usando tdd", "29-03-2019", "alta");
+    }
+
+}
