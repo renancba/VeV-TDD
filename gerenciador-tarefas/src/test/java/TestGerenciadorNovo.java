97fdedad3d98dc2d10844c5ef2fd495c9eb3fa9c
Renan Carneiro <55746096+renancba@users.noreply.github.com>
2023-09-25 22:21:55 -0300

    add: Testes JUNIT5 and Jacoco

--- gerenciador-tarefas/src/test/java/TestGerenciadorNovo.java
+++ gerenciador-tarefas/src/test/java/TestGerenciadorNovo.java
@@ -1,65 +1,95 @@
 import org.example.Enum.Prioridade;
 import org.example.controller.GerenciadorTarefas;
 import org.example.model.Tarefa;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.*;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class TestGerenciadorTarefas {
+public class TestGerenciadorNovo {
 
     private GerenciadorTarefas gerenciador;
 
-    @Before
+    @BeforeEach
     public void init() {
         this.gerenciador = new GerenciadorTarefas();
     }
 
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Init {
+    }
+
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Cleanup {
+    }
+
+    @Init
     @Test
     public void testCriarTarefa() {
         Tarefa tarefaTest = new Tarefa("fazer testes", "fazer testagem usando tdd", LocalDate.of(2018, 9, 28), Prioridade.ALTA);
         assertEquals("fazer testes", tarefaTest.getTitulo());
         assertEquals("fazer testagem usando tdd", tarefaTest.getDescricao());
-        assertEquals( LocalDate.of(2018, 9, 28), tarefaTest.getDataVencimento());
+        assertEquals(LocalDate.of(2018, 9, 28), tarefaTest.getDataVencimento());
         assertEquals(Prioridade.ALTA, tarefaTest.getPrioridade());
     }
 
+    @Cleanup
     @Test
-    public void testGerenciadorTarefa(){
+    public void testGerenciadorTarefa() {
         gerenciador.adicionar("fazer testes", "fazer testagem usando tdd", LocalDate.of(2018, 9, 28), Prioridade.ALTA);
         assertEquals(1, gerenciador.listar().size());
     }
+
+    @Cleanup
     @Test
-    public void testDeleteTarefaIndex(){
+    public void testDeleteTarefaIndex() {
         gerenciador.adicionar("fazer testes", "fazer testagem usando tdd", LocalDate.of(2018, 9, 28), Prioridade.ALTA);
         gerenciador.removerIndex(0);
+        assertEquals(0, gerenciador.listar().size());
     }
+
+    @Cleanup
     @Test
-    public void testEditTitutlo(){
-        gerenciador.adicionar("fazer testes", "fazer testagem usando tdd", LocalDate.of(2018, 9, 28), Prioridade.ALTA);
+    public void testEditTitutlo() {
+        Tarefa tarefa = gerenciador.adicionar("fazer testes", "fazer testagem usando tdd", LocalDate.of(2018, 9, 28), Prioridade.ALTA);
         gerenciador.editarTitulo(0, "Fazendo os testes");
+        assertEquals("Fazendo os testes", tarefa.getTitulo());
     }
+
+    @Cleanup
     @Test
-    public void testEditDescricao(){
-        gerenciador.adicionar("fazer testes", "fazer testagem usando tdd", LocalDate.of(2018, 9, 28), Prioridade.ALTA);
+    public void testEditDescricao() {
+        Tarefa tarefa = gerenciador.adicionar("fazer testes", "fazer testagem usando tdd", LocalDate.of(2018, 9, 28), Prioridade.ALTA);
         gerenciador.editarDescricao(0, "Fazendo a testagem usando tdd");
+        assertEquals("Fazendo a testagem usando tdd", tarefa.getDescricao());
     }
+
+    @Cleanup
     @Test
-    public void testEditData(){
-        gerenciador.adicionar("fazer testes", "fazer testagem usando tdd", LocalDate.of(2018, 9, 28), Prioridade.ALTA);
+    public void testEditData() {
+        Tarefa tarefa = gerenciador.adicionar("fazer testes", "fazer testagem usando tdd", LocalDate.of(2018, 9, 28), Prioridade.ALTA);
         gerenciador.editarData(0, LocalDate.of(2019, 9, 18));
+        assertEquals(LocalDate.of(2019, 9, 18), tarefa.getDataVencimento());
     }
+
+    @Cleanup
     @Test
-    public void testEditPrioridade(){
-        gerenciador.adicionar("fazer testes", "fazer testagem usando tdd", LocalDate.of(2018, 9, 28), Prioridade.ALTA);
+    public void testEditPrioridade() {
+        Tarefa tarefa = gerenciador.adicionar("fazer testes", "fazer testagem usando tdd", LocalDate.of(2018, 9, 28), Prioridade.ALTA);
         gerenciador.editarPrioridade(0, Prioridade.INTERMEDIARIA);
+        assertEquals(Prioridade.INTERMEDIARIA, tarefa.getPrioridade());
     }
 
     @Test
-    public void testGetTarefasOrdenadas(){
+    public void testGetTarefasOrdenadas() {
         Tarefa tarefa1 = gerenciador.adicionar("tarefa 1", "descrição 1", LocalDate.of(2023, 8, 31), Prioridade.ALTA);
         Tarefa tarefa2 = gerenciador.adicionar("tarefa 2", "descrição 2", LocalDate.of(2023, 9, 30), Prioridade.INTERMEDIARIA);
         Tarefa tarefa3 = gerenciador.adicionar("tarefa 3", "descrição 3", LocalDate.of(2023, 8, 31), Prioridade.BAIXA);
@@ -73,44 +103,37 @@ public class TestGerenciadorTarefas {
         assertEquals(tarefa3, tarefasOrdenadas.get(2));
         assertEquals(tarefa5, tarefasOrdenadas.get(3));
         assertEquals(tarefa2, tarefasOrdenadas.get(4));
-
     }
 
     @Test
     public void testRemoverTarefaComPrioridadeBaixaVencida() {
-        // Caso de teste 1
         Tarefa tarefa = gerenciador.adicionar("tarefa 1", "descrição 1", LocalDate.of(2022, 8, 31), Prioridade.BAIXA);
         boolean result = gerenciador.removerIndex(0);
-        assertEquals(true, result);
+        assertTrue(result);
     }
 
     @Test
     public void testRemoverTarefaComPrioridadeBaixaDentroPrazo() {
-        // Caso de teste 2
         Tarefa tarefa = gerenciador.adicionar("tarefa qualquer", "descrição qualquer", LocalDate.of(2023, 8, 31), Prioridade.BAIXA);
         boolean result = gerenciador.removerIndex(0);
-        assertEquals(true, result);
+        assertTrue(result);
     }
 
     @Test
     public void testExistenciaTarefaComPrioridadeBaixaVencida() {
-        // Caso de teste 3
         Tarefa tarefa = gerenciador.adicionar("tarefa qualquer", "descrição qualquer", LocalDate.of(2022, 8, 31), Prioridade.BAIXA);
         assertEquals(1, gerenciador.listar().size());
     }
 
     @Test
     public void testExistenciaTarefaComPrioridadeAltaFutura() {
-        // Caso de teste 4
         Tarefa tarefa = gerenciador.adicionar("tarefa qualquer", "descrição qualquer", LocalDate.of(2024, 8, 31), Prioridade.ALTA);
         assertEquals(1, gerenciador.listar().size());
     }
 
     @Test
     public void testExistenciaTarefaComPrioridadeMediaVencida() {
-        // Caso de teste 5
         Tarefa tarefa = gerenciador.adicionar("tarefa qualquer", "descrição qualquer", LocalDate.of(2022, 8, 31), Prioridade.INTERMEDIARIA);
         assertEquals(1, gerenciador.listar().size());
     }
-
 }
